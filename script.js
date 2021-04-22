5845ddf72106a29f24ec7b3b8793b5cfa8e2470f
Lasse Haslund <tofthaslund@gmail.com>
2021-04-22 16:14:23 +0100

    function to make countdown work

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ const items = document.querySelectorAll('.deadline-format h4');
 
 
 let futureDate = new Date(2021,11,30,7,30,0);
-console.log(futureDate);
+
 // year
 const year = futureDate.getFullYear();
 // hours
@@ -44,3 +44,49 @@ const weekday = weekdays[futureDate.getDay()];
 
 
 dateNy.textContent = `New Years eve party starts ${weekday}, ${date} ${month} ${year} at ${hours}:${minutes}pm`
+
+// furture timein ms
+const futureTime = futureDate.getTime();
+
+
+function getRemainingTime(){
+    const today = new Date().getTime();
+    const t = futureTime - today;
+    // 1s = 1000ms
+    // 1m = 60s
+    // 1hr = 60min
+    // 1day = 24hr
+
+    // values in ms
+    const oneDay = 24 * 60 * 60 * 1000;
+    const oneHour = 60 * 60 * 1000;
+    const oneMinute = 60 * 1000;
+    // calculate all values
+    let days = Math.floor(t / oneDay);
+    let hours = Math.floor((t % oneDay) / oneHour );
+    let min = Math.floor((t % oneHour) / oneMinute);
+    let sec = Math.floor((t % oneMinute) / 1000);
+    
+    // Set values array
+    const values = [days,hours,min,sec];
+
+    // sets a 0 if number is less than 10
+    function format(item){
+        if(item < 10){
+            return item = `0${item}`
+        }
+        return item;
+    }
+
+
+    items.forEach(function(item, index){
+        item.innerHTML = format(values[index]);
+    });
+    if(t < 0){
+        clearInterval(countdown);
+        deadline.innerHTML = `<h4 class="expired">Sorry the party is over</h4>`
+    }
+}
+// countdown
+let countdown = setInterval(getRemainingTime, 1000)
+getRemainingTime();
